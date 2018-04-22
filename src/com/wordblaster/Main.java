a6c93d4c5d28d17bd3264be78e940d77b33316a3
Shink0 <none>
2018-04-22 12:45:15 +0200

    No need of throwing IOException

--- src/com/wordblaster/Main.java
+++ src/com/wordblaster/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Launcher launcher = new Launcher();
     }
 
