dc80ed41851157817eee3078b6eaabf6f5f38788
Shink0 <mihael.s.97@gmail.com>
2018-04-23 11:22:56 +0200

    Working on settings feature

---
+++ src/com/wordblaster/renderer/DisplaySettings.java
@@ -0,0 +1,27 @@
+package com.wordblaster.renderer;
+
+public class DisplaySettings {
+
+    private int resolution_h;
+    private int resolution_l;
+
+    public DisplaySettings() {
+    }
+
+    public int getResolution_h() {
+        return resolution_h;
+    }
+
+    public void setResolution_h(int resolution_h) {
+        this.resolution_h = resolution_h;
+    }
+
+    public int getResolution_l() {
+        return resolution_l;
+    }
+
+    public void setResolution_l(int resolution_l) {
+        this.resolution_l = resolution_l;
+    }
+
+}
